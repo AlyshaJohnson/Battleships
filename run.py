304bc23cefee8550fba12cbfad507ff4fb106e95
AlyshaJohnson <alysha.johnson@live.co.uk>
2022-07-13 20:43:59 +0000

    variable and function scoping

--- run.py
+++ run.py
@@ -1,3 +1,45 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# Imports
+
+# Named variables
+player_name = input("Name: ")
+player_hit_count = 0
+player_ships_count = 5
+computer_hit_count = 0
+computer_ships_count = 0
+
+# Functions
+def create_computer_boards():
+    # creates computer guess board and ship placement boards
+    pass
+
+def load_blank_player_boards():
+    # creates blank player guess and ship placement boards
+    pass
+
+def place_ships():
+    # allows player to place ships
+    pass
+
+def reset_player_board():
+    # allows player to reset their game board
+    pass
+
+def print_boards():
+    # prints player boards to terminal
+    pass
+
+def player_guess():
+    # player makes guess (hit and miss)
+    pass
+
+def computer_guess():
+    # computer makes guess (hit and miss)
+    pass
+
+def end_game():
+    # ends game (win or lose)
+    pass
